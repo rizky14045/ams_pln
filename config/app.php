3d5f72ab641b1e5aac6c99380d1c2d051aa6e121
rizkyabhimantra <rizky@abhimantra.co.id>
2026-09-14 14:10:40 +0700

    add cors

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
